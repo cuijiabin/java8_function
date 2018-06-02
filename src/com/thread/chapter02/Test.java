8e9927e178195e19c83050cd3b82c1da9f04d10c
cuijiabin <cuijia_bin@126.com>
2018-06-02 22:22:39 +0800

    add t2

--- src/com/thread/chapter02/Test.java
+++ src/com/thread/chapter02/Test.java
@@ -3,7 +3,8 @@ package com.thread.chapter02;
 public class Test {
 
     public static void main(String[] args) {
-        test1();
+//        test1();
+        test2();
     }
 
     public static void test1() {
@@ -13,5 +14,15 @@ public class Test {
 
         ThreadB b = new ThreadB(numRef);
         b.start();
+    }
+
+    public static void test2() {
+        HasSelfPrivateNum2 numRef = new HasSelfPrivateNum2();
+
+        ThreadA2 athread = new ThreadA2(numRef);
+        athread.start();
+
+        ThreadB2 bthread = new ThreadB2(numRef);
+        bthread.start();
     }
 }
