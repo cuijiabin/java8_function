c863b282b51cb70afa70bc2c4a4bca868913b613
cuijiabin <cuijia_bin@126.com>
2018-06-25 10:11:06 +0800

    md 语法添加

--- src/com/thread/abstract.md
+++ src/com/thread/abstract.md
@@ -1,5 +1,15 @@
 1.java 多线程都有哪些内容？
 
-#### 重要的类 Thread Runable
-#### 关键字 synchronize volatile
-#### JUC包 原子类 锁 集合 线程池
+重要的类 Thread Runable <br>
+关键字 synchronize volatile <br>
+JUC包 原子类 锁 集合 线程池 <br>
+
+> java.lang.Runnable <br>
+> java.lang.Thread <br>
+> java.lang.ThreadDeath <br>
+> java.lang.ThreadGroup <br>
+> java.lang.ThreadLocal
+
++ 列表1
++ 列表2
++ 列表3 
--- src/com/thread/chapter01/TestDemo.java
+++ src/com/thread/chapter01/TestDemo.java
@@ -4,22 +4,14 @@ import org.junit.Test;
 
 public class TestDemo {
 
-    public synchronized void doSth() {
-        System.out.println("Hello World");
-    }
-
-    public void doSth1() {
-        synchronized (TestDemo.class) {
-            System.out.println("Hello World");
-        }
-    }
-
     public static void main(String[] args) {
+
         System.out.println(Thread.currentThread().getName());
         MyThread1 myThread1 = new MyThread1();
         myThread1.start();
         myThread1.run();
         System.out.println("运行结束了");
+
     }
 
     /**
